e85cc1aa40a2d9721810d5cb3258fb0737a9a0b5
Bastian Berrios Cruz <134324655+bastianBerrios@users.noreply.github.com>
2025-02-01 23:59:22 -0300

    Delete core directory

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cliente,Trabajador,Reserva,ClienteCuenta
-
-admin.site.register(Cliente)
-admin.site.register(Trabajador)
-admin.site.register(Reserva)
-admin.site.register(ClienteCuenta)
-# Register your models here.
